0199c156223d03d4836346c7c40f97b12b42f2bd
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-18 16:34:48 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  
+  def self.reset_all
+    OWNER.clear
+  end
+  
+  def initialize(name)
+    OWNER << self
+    @name = name
+    @pets = {:dogs => :cats}
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cat] << Cat.new(name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets = {:dogs => :cats []}
+  end
+
 end
