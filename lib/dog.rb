68396eb06715c67ac01187d91755c86bf5873e71
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-18 17:58:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,15 @@
 class Dog
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_reader :name
   
+    def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
   
+  def self.all
+      @@all
+ end
+end
